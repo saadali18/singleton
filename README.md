2a16312244a48aae4cb9c01a1abb4e2084ae9345
Saad Ali <pakwheels@pakwheelssmbp2.home>
2023-05-25 20:48:47 +0500

    main.c file added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# singleton
+# Singleton
+
+The Singleton design pattern is a creational design pattern that ensures that only one instance of a class is created and provides a global point of access to that instance. It is useful in scenarios where you want to limit the number of instances of a class to exactly one, such as when you want to control access to a shared resource or when you need a global object that can be accessed from anywhere in the program.
+
+
---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int data;
+} Singleton;
+
+static Singleton* instance = NULL;
+
+Singleton* getSingletonInstance() {
+    if (instance == NULL) {
+        instance = (Singleton*)malloc(sizeof(Singleton));
+        instance->data = 0;
+    }
+    return instance;
+}
+
+int main() {
+    Singleton* singleton = getSingletonInstance();
+    printf("Singleton data: %d\n", singleton->data);
+
+    singleton->data = 42;
+
+    Singleton* anotherSingleton = getSingletonInstance();
+    printf("Singleton data: %d\n", anotherSingleton->data);
+
+    return 0;
+}
